bfa4a671b43dbd8d954d22d92c5cb3a6110223aa
Your Name <you@example.com>
2015-12-28 20:46:19 +0600

    init

---
+++ Demo/src/MyPackage/Main.java
@@ -0,0 +1,17 @@
+/*
+ * set main
+ */
+
+package MyPackage;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello wold");
+	}
+
+}
